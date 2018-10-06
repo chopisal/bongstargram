6ea1613153e514eccc41778b86e8130219c4d340
bong <chopisal@gmail.com>
2018-10-06 18:57:54 +0900

    images add

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
+  state = {
+    isLoadingComplete: true
+  };
   render() {
+    const { isLoadingComplete } = this.state;
+    if (!isLoadingComplete) {
+      return <AppLoading />;
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +23,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
