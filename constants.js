b0beb5ec234e017827adf94bcfe1c07c98083e14
bong <chopisal@gmail.com>
2018-10-07 14:51:38 +0900

    login

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const API_URL = "http://localhost:4000";
+export const FB_APP_ID = "12345";
---
+++ redux/modules/user.js
@@ -0,0 +1,148 @@
+// Imports
+
+import { API_URL, FB_APP_ID } from "../../constants";
+import { AsyncStorage } from "react-native";
+import { Permissions, Notifications, Facebook } from "expo";
+
+// Actions
+
+const LOG_IN = "LOG_IN";
+const LOG_OUT = "LOG_OUT";
+const SET_USER = "SET_USER";
+
+// Action Creators
+
+function setLogIn(token) {
+  return {
+    type: LOG_IN,
+    token
+  };
+}
+
+function setUser(user) {
+  return {
+    type: SET_USER,
+    user
+  };
+}
+
+function logOut() {
+  return { type: LOG_OUT };
+}
+
+// API Actions
+function login(username, password) {
+  return dispatch => {
+    return fetch(`${API_URL}/rest-auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (json.user && json.token) {
+          dispatch(setLogIn(json.token));
+          dispatch(setUser(json.user));
+          return true;
+        } else {
+          return false;
+        }
+      });
+  };
+}
+
+function facebookLogin() {
+  return async dispatch => {
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+      FB_APP_ID,
+      {
+        permissions: ["public_profile", "email"]
+      }
+    );
+    if (type === "success") {
+      return fetch(`${API_URL}/users/login/facebook/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          access_token: token
+        })
+      })
+        .then(response => response.json())
+        .then(json => {
+          if (json.user && json.token) {
+            dispatch(setLogIn(json.token));
+            dispatch(setUser(json.user));
+            return true;
+          } else {
+            return false;
+          }
+        });
+    }
+  };
+}
+
+// Initial State
+
+const initialState = {
+  isLoggedIn: false
+};
+
+// Reducer
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case LOG_IN:
+      return applyLogin(state, action);
+    default:
+      return state;
+  }
+}
+
+// Reducer Functions
+
+function applyLogin(state, action) {
+  const { token } = action;
+  return {
+    ...state,
+    isLoggedIn: true,
+    token
+  };
+}
+
+function applyLogOut(state, action) {
+  AsyncStorage.clear();
+  return {
+    ...state,
+    isLoggedIn: false,
+    token: ""
+  };
+}
+
+function applySetUser(state, action) {
+  const { user } = action;
+  return {
+    ...state,
+    profile: user
+  };
+}
+
+// Exports
+
+const actionCreators = {
+  login,
+  facebookLogin,
+  logOut
+};
+
+export { actionCreators };
+
+// Default Reducer Export
+
+export default reducer;
